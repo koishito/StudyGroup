7d1c4d1711a2efb13cfca17c9c1a4eb012f7abc8
koishito <64292509+koishito@users.noreply.github.com>
2020-12-28 17:23:13 +0900

    finished sleeep.js

--- StudyJavascript/sleep.js
+++ StudyJavascript/sleep.js
@@ -1,3 +1,17 @@
+// ビジーwaitを使う方法
+function sleep(waitMsec) {
+  var startMsec = new Date();
+ 
+  // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+  while (new Date() - startMsec < waitMsec);
+}
+ 
+sleep(5000);
+ 
+// 5秒後にメッセージを表示
+console.log('5秒経過しました！');
+
+// async、awaitを使う方法
 async function start() {
  
   await sleep(5000);
