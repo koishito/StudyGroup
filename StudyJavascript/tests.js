7f8cc66567d3767f508d179ede63b6109d9075ff
koishito <64292509+koishito@users.noreply.github.com>
2020-12-31 17:09:30 +0900

    no message

--- StudyJavascript/tests.js
+++ StudyJavascript/tests.js
@@ -14,6 +14,24 @@ var temp = `/^https:\/\/ncode.syosetu.com\/n\d{4}\u{2}\//`.replace(/[.*+?^=!:${}
 
 var temp = `/^https:\\/\\/ncode.syosetu.com\\/n\\d{4}\\u{2}\\//`.replace(/[.*+?^=!:${}()|[\]\/\\]/g, '\\$&');
 var temp = `/^https:\\/\\/ncode.syosetu.com\\/n\\d{4}\\u{2}\\//`.replace(/\\/g, '\\$&');
-console.log(temp);
-console.log(RegExp(temp));
+// console.log(temp);
+// console.log(RegExp(temp));
 
+// console.log("https://ncode.syosetu.com/n2485fu/12/".match(/^https:\/\/ncode.syosetu.com\/n\d{4}[a-z]{2}\/.+$/));
+// console.log("https://ncode.syosetu.com/n2485fu/12/".match(/^https:\/\/ncode.syosetu.com\/n\d{4}[a-z]{2}\//));
+
+// let a = 2;
+// let b = 'hello';
+// a += 3;
+// console.log(a); // addition
+// // expected output: 5
+// b += ' world';
+// console.log(b); // concatenation
+// // expected output: "hello world"
+
+
+// var temp = "test";
+// var temp2 = "'this is temp.'";
+// console.log(temp2.replace(/temp/, temp))
+
+console.log('0123'.slice(1));
